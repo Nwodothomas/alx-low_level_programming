594cec75642de89fcf3754d35a8892f456a4274e
Thomas Nwodo <nwodothomas@gmail.com>
2022-09-08 20:20:21 -0700

    hello inputs updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,8 @@
  *
  * Return: Always 0 (success)
  */
-int main(void)
+int main(void)	
 {
- puts("\"programming is like building multilinqual");
-
-  return(0);
+	puts("\"Programming is like building a multilingual puzzle);
+	return(0);
 }
