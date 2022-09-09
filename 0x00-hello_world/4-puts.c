e66bf03dd81ef0ab144624cf87f56747271810ee
Thomas Nwodo <nwodothomas@gmail.com>
2022-09-08 20:09:43 -0700

    hello input

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - printing in c
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+ puts("\"programming is like building multilinqual");
+
+  return(0);
+}
