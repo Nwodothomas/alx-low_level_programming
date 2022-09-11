dc0e3d84064537ea44770ec56d6708a8b131122a
Thomas Nwodo <nwodothomas@gmail.com>
2022-09-11 16:29:46 -0700

    ommit some alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - ommot some alphabets
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar (c);
+		}
+
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
