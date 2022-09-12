320305c0ddb938fe90713af61d0fefc8f5f8fd5c
Thomas Nwodo <nwodothomas@gmail.com>
2022-09-11 19:06:17 -0700

    combinations of numbers with comma

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,13 +10,12 @@ int main(void)
 
 	for(c = '0'; c <= '9'; c++)
 	{
-		putchar (c);
-
+		putchar(c);
 		if (c != '9')
 		{
 		
-			putchar (',');
-			putchar (',');
+			putchar(',');
+			putchar('');
 		}
 		
 	}
