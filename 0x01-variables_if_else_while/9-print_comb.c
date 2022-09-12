2eec25a1359c0707d0d16b31f04332e81dfd56ed
Thomas Nwodo <nwodothomas@gmail.com>
2022-09-11 18:06:33 -0700

    numbers with comma

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-	char c;
+	int n;
 
-	for (c = '0'; c <= '9'; c++)
+	for (n = '0'; n = '9'; c++)
 	{
-		putchar (c);
+		putchar (n);
 
-		if (c != '9')
+		if (n != '9')
 		{
 			putchar (',');
 			putchar (',');
